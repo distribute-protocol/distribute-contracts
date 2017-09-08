efcb46cd29ab1d038678cd5d2d59fe02dd0713a6
Jessica Marshall <jessica.marshall14@gmail.com>
2017-09-08 11:35:25 -0400

    updated functions, implemented createProject

--- tokenprice_simulation.py
+++ tokenprice_simulation.py
@@ -66,31 +66,55 @@ class TokenHolder:
             print('burn return per token:', price, 'ETH')
             print('total burn return:', total_price, 'ETH\n')
 
-    #def stakeTokens(self, project, )
+    def createProject(self, _cost, _id):
+        Project(_cost, _id)
+
+    #def stakeProject():
+
+    #def validateProject():
+
+    #def voteProject():
+
+class Worker:
+
+    def __init__(self):
+        self.worktokens = 1     #each worker is initialized with one token
+
+    #def stakeProject():
+
+    #def pickTask();
+
+    #def voteProject():
+
 
 class Project:
 
-        def __init__(self, cost):
-            self.project_cost = cost
-            self.project_stage = 'proposed'     #enumerate this?
+        def __init__(self, _cost, _id):         #_id should be the name of the project
+            self.project_cost = _cost
+            self.project_state = 'proposed'     #enumerate this?
+            self.project_id = _id
             #proposed, open, active, completed, [incomplete], validated/[failed]
             if (burnPrice() == 0):
                 self.project_tokens = None
             else:
-                self.project_tokens = (cost / burnPrice())    #at initialization, number of tokens needed
-            print('project initialized')
+                self.project_tokens = (_cost / burnPrice())    #at initialization, number of tokens needed
+            print('project', self.project_id, 'initialized')
             print('total project cost: ', self.project_cost, 'ETH')
             print('tokens required at initialization: ', self.project_tokens, '\n')
+
+        #def changeState():
+
+        #def refundProposer():
 
 
 
 #define instances of TokenHolder class
 Jessica = TokenHolder()
 Ashoka = TokenHolder()
-node442 = Project(10)
+#node442 = Project(10, 'project 442')
 #example class method calls
-Jessica.mintTokens(1)
-node443 = Project(10)
+Jessica.createProject(10, 'node443')
+#node443 = Project()
 #Ashoka.mintTokens(5)
 Jessica.burnTokens(11)
 #Jessica.burnTokens(9)
