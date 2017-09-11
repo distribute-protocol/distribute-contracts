a1fc4a3617b468c62cd91a2e5a4ed36afd8080f2
Jessica Marshall <jessica.marshall14@gmail.com>
2017-09-11 14:26:30 -0400

    introduced array to keep track of projects and actors

--- tokenprice_simulation.py
+++ tokenprice_simulation.py
@@ -1,12 +1,19 @@
 #Jessica Marshall
-#Distribute
-#token pricing simulation
+#Distribute simulation
 
+#import packages
+import numpy as np
 
 #initial values/global variables
 constant = 1
 pool_ETH = 0
 token_supply = 0
+worker_id = 0
+tokenholder_id = 0
+project_id = 0
+    #hard code project array for simulation
+    #first dim are projects, second dim are token holders, third dim are workers
+projects = np.array((100, 100, 100))
 
 
 #mintPrice returns the amount of ether 1 token can be minted for
@@ -30,6 +37,11 @@ class TokenHolder:
     def __init__(self):
         self.tokens = 0
         self.investment = 0     #a negative investment is a profit
+
+        global tokenholder_id
+        self.tokenholderid = tokenholder_id     #to keep track of token holders
+        tokenholder_id += 1
+        #print('initialized TokenHolder', self.tokenholderid)
 
     #mintTokens allows a TokenHolder to mint tokens for themself
     def mintTokens(self, num_tokens):
@@ -69,9 +81,10 @@ class TokenHolder:
 
     def createProject(self, _cost, _id):
         Project(_cost, _id)
+        #add task functionality here!
 
-    #def stakeProject(self, num_tokens, ):
-        #lock up tokens
+    #def stakeProject(self, num_tokens, _projectid):
+        #lock up tokens in array
 
     #def validateProject():
 
@@ -82,6 +95,11 @@ class Worker:
     def __init__(self):
         self.worktokens = 1     #each worker is initialized with one token
 
+        global worker_id
+        self.workerid = worker_id
+        worker_id += 1
+        print('initialized worker', self.workerid)
+
     #def stakeProject():
 
     #def pickTask();
@@ -91,26 +109,31 @@ class Worker:
 
 class Project:
 
-        def __init__(self, _cost, _id):         #_id should be the name of the project
+        def __init__(self, _cost):         #_id should be the name of the project
             self.project_cost = _cost
-            self.project_state = 'proposed'     #enumerate this?
-            self.project_id = _id
+            self.project_state = 'proposed'
+
+            global project_id
+            self.projectid = project_id
+            project_id += 1
+            print('initialized project', self.projectid)
+
             #proposed, open, active, completed, [incomplete], validated/[failed]
             if (burnPrice() == 0):
                 self.project_tokens = None
             else:
                 self.project_tokens = (_cost / burnPrice())    #at initialization, number of tokens needed
-            print('project', self.project_id, 'initialized')
+            print('project', self.projectid, 'initialized')
             print('total project cost: ', self.project_cost, 'ETH')
             print('tokens required at initialization: ', self.project_tokens, '\n')
 
         def changeState(self):
             state = ['proposed', 'active', 'open', 'complete', 'validated']
             if self.project_state in state:
-                print('old state of', self.project_id, 'is', self.project_state)
+                print('old state of', self.projectid, 'is', self.project_state)
                 index = state.index( self.project_state)
                 self.project_state = state[index + 1]
-                print('new state of', self.project_id, 'is', self.project_state)
+                print('new state of', self.projectid, 'is', self.project_state)
             else:
                 print('state not in state list');
 
@@ -120,7 +143,8 @@ class Project:
 
 def main():
     Jessica = TokenHolder()
-    Project1 = Project(1, 'project1')
+    Ashoka = TokenHolder()
+    Project1 = Project(1)
     Project1.changeState()
     #create an array of token holders
     #have a few of them createProjects
