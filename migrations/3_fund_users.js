405ca39d66bd08ce28f0d01d732ff05250ef287a
Jessica Marshall <jessica.marshall14@gmail.com>
2017-09-20 15:38:07 -0400

    working on truffle javascript

--- migrations/3_fund_users.js
+++ migrations/3_fund_users.js
@@ -2,17 +2,22 @@ var TokenHolderRegistry = artifacts.require("./TokenHolderRegistry.sol");
 
 module.exports = function(deployer) {
     var accounts = web3.eth.accounts;
+    console.log(accounts.length)
     var account1 = accounts[0];
     var account2 = accounts[1];
 
     deployer.then(function(){
         return TokenHolderRegistry.deployed()
     }).then(function(instance){
-        for(i=1; i < accounts.length; i++) {
-            console.log(web3.eth.getBalance(accounts[i]))
-          //  instance.mint(10, {from: accounts[i]})
-          //  console.log(web3.eth.getBalance(account[i]))
+        for(i=0; i < accounts.length; i++) {
+            console.log(web3.eth.getBalance(accounts[i]).toNumber())
+            instance.mint(10, {from: accounts[i]})
+            return TokenHolderRegistry.deployed()
         }
+    }).then(function(){
+        for(i=0; i < accounts.length; i++) {
+            console.log(web3.eth.getBalance(accounts[i]))
         // instance.transfer(demoAddress, 1, {from: accounts[0]})
+        }
     })
 }
