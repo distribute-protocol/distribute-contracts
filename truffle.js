d3638648535c09675108b277a033c5eae20b02eb
Jessica Marshall <jessica.marshall14@gmail.com>
2017-10-24 10:40:54 -0400

    removed infura api key - unnecessary

--- truffle.js
+++ truffle.js
@@ -3,18 +3,10 @@ const fs = require('fs')
 
 // first read in the secrets.json to get our mnemonic
 let mnemonic;
-let infura_apikey;
 if (fs.existsSync('secrets.json')) {
   mnemonic = JSON.parse(fs.readFileSync('secrets.json', 'utf8'));
 } else {
   mnemonic = '';
-}
-
-if (fs.existsSync('infura_apikey.json')) {
-  infura_apikey = JSON.parse(fs.readFileSync('infura_apikey.json', 'utf8'));
-} else {
-  console.log('no infura_apikey.json found. You can only deploy to the testrpc.');
-  infura_apikey = '';
 }
 
 module.exports = {
@@ -25,13 +17,13 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     ropsten: {
-      provider: new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/'+infura_apikey),
+      provider: new HDWalletProvider(mnemonic, 'https://ropsten.infura.io'),
       network_id: "3",
       gas: 4700000,
       gasPrice: 20000000000,
     },
     rinkeby: {
-      provider: new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/'+infura_apikey),
+      provider: new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io'),
       network_id: "*",
       gas: 4700000,
       gasPrice: 20000000000,
