efe8508d0b86afc17962abb3359e363e6e07ba15
Jessica Marshall <jessica.marshall14@gmail.com>
2017-10-31 17:09:49 -0400

    removed network: test

--- truffle.js
+++ truffle.js
@@ -11,11 +11,6 @@ if (fs.existsSync('secrets.json')) {
 
 module.exports = {
   networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
-    },
     ropsten: {
       provider: new HDWalletProvider(mnemonic, 'https://ropsten.infura.io'),
       network_id: "3",
