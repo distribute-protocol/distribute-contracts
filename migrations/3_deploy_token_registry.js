720dd0a320a07f115cbc95444b34bf3a1024a607
shkfnly <ashoka.finley@gmail.com>
2017-12-12 00:10:08 -0500

    remove unneccessary file

--- migrations/3_deploy_token_registry.js
+++
@@ -1,7 +0,0 @@
-const TokenRegistry = artifacts.require('TokenRegistry')
-
-module.exports = function (deployer) {
-  deployer.then(function () {
-    return deployer.deploy(TokenRegistry, {gas: 5718615})
-  })
-}
