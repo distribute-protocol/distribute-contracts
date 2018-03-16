9371c063ff73ab2b159914c0b5fc96caae75b076
shkfnly <ashoka.finley@gmail.com>
2018-03-16 11:12:14 +0530

    stream line deployments

--- migrations/3_deploy_contracts.js
+++ migrations/3_deploy_contracts.js
@@ -11,23 +11,16 @@ const Division = artifacts.require('library/Division')
 */
 
 module.exports = function (deployer) {
-  deployer.deploy(ProjectLibrary)
   deployer.deploy(Division)
   deployer.link(Division, [DistributeToken, ProjectLibrary, ReputationRegistry, TokenRegistry])
+  deployer.deploy(ProjectLibrary)
   deployer.link(ProjectLibrary, [TokenRegistry, ReputationRegistry, ProjectRegistry])
-  deployer.then(function () {
-    return deployer.deploy(TokenRegistry)
-  }).then(function () {
-    return deployer.deploy(ReputationRegistry)
-  }).then(function () {
-    return deployer.deploy(ProjectRegistry)
-  }).then(function () {
-    return deployer.deploy(DistributeToken, TokenRegistry.address, ReputationRegistry.address)
-  }).then(function () {
-    return deployer.deploy(PLCRVoting, TokenRegistry.address, ReputationRegistry.address, ProjectRegistry.address)
-  }).then(function () {
-    return ProjectRegistry.deployed()
-  }).then(function (instance) {
+  deployer.deploy(TokenRegistry)
+  deployer.deploy(ReputationRegistry)
+  deployer.deploy(ProjectRegistry)
+  deployer.deploy(DistributeToken, TokenRegistry.address, ReputationRegistry.address)
+  deployer.deploy(PLCRVoting, TokenRegistry.address, ReputationRegistry.address, ProjectRegistry.address)
+  ProjectRegistry.deployed().then(function (instance) {
     return instance.init(DistributeToken.address, TokenRegistry.address, ReputationRegistry.address, PLCRVoting.address)
   }).then(function () {
     return TokenRegistry.deployed()
