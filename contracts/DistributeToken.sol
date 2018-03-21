55a2bd00046cc22de0f3d30b5d97945e9cd38435
shkfnly <ashoka.finley@gmail.com>
2018-03-21 11:27:25 -0400

    look solidity pragma

--- contracts/DistributeToken.sol
+++ contracts/DistributeToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.19;
+pragma solidity 0.4.19;
 
 import "./library/StandardToken.sol";
 import "./library/SafeMath.sol";
--- contracts/Project.sol
+++ contracts/Project.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.19;
+pragma solidity 0.4.19;
 
 import "./ProjectRegistry.sol";
 import "./ReputationRegistry.sol";
