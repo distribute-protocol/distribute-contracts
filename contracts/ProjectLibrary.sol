4caddf0800f46da37e9d33b1955a174790e0679a
ssircar <swarnima.sircar@gmail.com>
2018-08-03 12:36:07 -0400

    reward taskclaimer logs

--- contracts/ProjectLibrary.sol
+++ contracts/ProjectLibrary.sol
@@ -26,6 +26,7 @@ library ProjectLibrary {
     event ReputationRefund(address projectAddress, address staker, uint256 refund);
     event LogTaskVote(address taskAddress, address projectAddress, uint pollNonce);
     event LogTaskValidated(address taskAddress, address projectAddress, bool confirmation);
+    event LogClaimTaskReward(address projectAddress, uint256 index, address taskClaimer, uint256 weiReward, uint256 reputationReward);
     // =====================================================================
     // UTILITY
     // =====================================================================
@@ -370,6 +371,7 @@ library ProjectLibrary {
         task.setTaskReward(0, 0, _claimer);
         project.transferWeiReward(_claimer, weiReward);
         return reputationReward;
+        emit LogClaimTaskReward(_projectAddress, _index, _claimer, weiReward, reputationReward);
     }
 
     // =====================================================================
